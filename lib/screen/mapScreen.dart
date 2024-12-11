3fef3ca7ae4142d2ef4f37eaef30508fa719ab39
carlaaotero <carla.otero@estudiantat.upc.edu>
2024-12-11 17:04:39 +0100

    Mapa bonic

--- lib/screen/mapScreen.dart
+++ lib/screen/mapScreen.dart
@@ -8,166 +8,143 @@ import 'package:flutter_application_1/models/ubi.dart';
 class MapScreen extends StatelessWidget {
   final UbiController ubiController =
       Get.put(UbiController()); // Obtenim el controlador
-  Rx<UbiModel?> selectedUbi = Rx<UbiModel?>(
-      null); // Afegim una variable per emmagatzemar la ubicació seleccionada
+  RxList<UbiModel> selectedUbications = RxList<UbiModel>([]); // Llista d'ubicacions seleccionades
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Mapa'),
-      ),
-      body: Row(
-        children: [
-          // El mapa
-          Expanded(
-            flex: 2,
-            child: Obx(() {
-              // Espera que les ubicacions siguin carregades
-              if (ubiController.isLoading.value) {
-                return Center(child: CircularProgressIndicator());
-              }
-
-              // Si no hi ha ubicacions, mostra un missatge
-              if (ubiController.ubis.isEmpty) {
-                return Center(child: Text('No hi ha ubicacions disponibles'));
-              }
-
-              // Generem el mapa amb els markers
-              return FlutterMap(
-                options: MapOptions(
-                  center: LatLng(41.382395521312176,
-                      2.1567611541534366), // Coordenades de Barcelona
-                  zoom: 13.0, // Nivell de zoom inicial
-                  onTap: (tapPosition, latLng) {
-                    selectedUbi.value =
-                        null; // Si fem clic fora d'un marker, es neteja la selecció
-                  },
-                ),
-                children: [
-                  TileLayer(
-                    urlTemplate:
-                        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                    subdomains: ['a', 'b', 'c'],
-                  ),
-                  MarkerLayer(
-                    markers: ubiController.ubis.map((ubi) {
-                      // Accedim a les coordenades
-                      final latitude =
-                          ubi.ubication['latitud'] ?? 41.382395521312176;
-                      final longitude =
-                          ubi.ubication['longitud'] ?? 2.1567611541534366;
-
-                      return Marker(
-                        point: LatLng(latitude, longitude),
-                        builder: (ctx) => GestureDetector(
-                          onTap: () {
-                            selectedUbi.value =
-                                ubi; // Actualitza la ubicació seleccionada
-                          },
-                          child: Icon(
-                            Icons.location_on,
-                            color: Colors.red,
-                            size: 30.0,
-                          ),
-                        ),
-                      );
-                    }).toList(), // Convertim el resultat en una llista
-                  ),
-                ],
-              );
-            }),
-          ),
-
-          // El container que es mostra a la dreta per mostrar la informació
-          Expanded(
-            flex: 1,
-            child: Obx(() {
-              final ubi = selectedUbi.value;
-              if (ubi == null) {
-                return Center(child: Text("Selecciona una ubicació"));
-              }
-              return Container(
-                padding: EdgeInsets.all(16.0),
-                color: Colors.grey[200],
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Nom: ${ubi.name}',
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold)),
-                    SizedBox(height: 10),
-                    Text('Tipus: ${ubi.tipo}', style: TextStyle(fontSize: 16)),
-                    SizedBox(height: 10),
-                    Text('Horari: ${ubi.horari}',
-                        style: TextStyle(fontSize: 16)),
-                    SizedBox(height: 10),
-                    Text('Comentari: ${ubi.comentari}',
-                        style: TextStyle(fontSize: 16)),
-                  ],
-                ),
-              );
-            }),
-          ),
-        ],
-      ),
-    );
-  }
-}
-/*
-class MapScreen extends StatelessWidget {
-  final UbiController ubiController = Get.put(UbiController()); // Obtenim el controlador
-  Rx<UbiModel?> selectedUbi = Rx<UbiModel?>(null);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Mapa'),
+        title: const Text('Mapa'),
+        backgroundColor: const Color(0xFF89AFAF),
       ),
       body: Obx(() {
         // Espera que les ubicacions siguin carregades
         if (ubiController.isLoading.value) {
-          return Center(child: CircularProgressIndicator());
+          return const Center(child: CircularProgressIndicator());
         }
 
         // Si no hi ha ubicacions, mostra un missatge
         if (ubiController.ubis.isEmpty) {
-          return Center(child: Text('No hi ha ubicacions disponibles'));
+          return const Center(child: Text('No hi ha ubicacions disponibles'));
         }
 
-        // Generem el mapa amb els markers
-        return FlutterMap(
-          options: MapOptions(
-            center: LatLng(41.382395521312176, 2.1567611541534366), // Coordenades de Barcelona
-            zoom: 13.0, // Nivell de zoom inicial
-          ),
+        return Stack(
           children: [
-            TileLayer(
-              urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-              subdomains: ['a', 'b', 'c'],
-            ),
-            MarkerLayer(
-              markers: ubiController.ubis.map((ubi) {
-                // Accedim a les coordenades
-                final latitude = ubi.ubication['latitud'] ?? 41.382395521312176;
-                final longitude = ubi.ubication['longitud'] ?? 2.1567611541534366;
+            // Mapa
+            FlutterMap(
+              options: MapOptions(
+                center: LatLng(41.382395521312176,
+                    2.1567611541534366), // Coordenades de Barcelona
+                zoom: 13.0, // Nivell de zoom inicial
+              ),
+              children: [
+                TileLayer(
+                  urlTemplate:
+                      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                  subdomains: ['a', 'b', 'c'],
+                ),
+                MarkerLayer(
+                  markers: ubiController.ubis.map((ubi) {
+                    // Accedim a les coordenades
+                    final latitude =
+                        ubi.ubication['latitud'] ?? 41.382395521312176;
+                    final longitude =
+                        ubi.ubication['longitud'] ?? 2.1567611541534366;
 
-                return Marker(
-                  point: LatLng(latitude, longitude),
-                  builder: (ctx) => Icon(
-                    Icons.location_on,
-                    color: Colors.red,
-                    size: 30.0,
-                  ),
-                );
-              }).toList(), // Convertim el resultat en una llista
+                    return Marker(
+                      point: LatLng(latitude, longitude),
+                      builder: (ctx) => GestureDetector(
+                        onTap: () {
+                          if (!selectedUbications.contains(ubi)) {
+                            selectedUbications.add(ubi); // Afegim a la llista
+                          }
+                        },
+                        child: Icon(
+                          Icons.place, // Icònica més estètica
+                          color: const Color.fromARGB(255, 133, 160, 160),
+                          size: 35.0, // Mida del marcador
+                        ),
+                      ),
+                    );
+                  }).toList(), // Convertim el resultat en una llista
+                ),
+              ],
             ),
+
+            // Mostra les caixes per a les ubicacions seleccionades
+            ...selectedUbications.map((ubi) {
+              int index = selectedUbications.indexOf(ubi);
+              return Positioned(
+                right: 16.0,
+                top: 16.0 + index * 160.0, // Espai suficient entre caixes
+                child: Container(
+                  width: 220.0,
+                  padding: const EdgeInsets.all(12.0),
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFB2D5D5),
+                    borderRadius: BorderRadius.circular(12.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        blurRadius: 6.0,
+                        offset: const Offset(3, 3),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            'Detalls',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              selectedUbications.remove(ubi); // Elimina la caixa
+                            },
+                            child: const Icon(
+                              Icons.close,
+                              size: 20.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        'Nom: ${ubi.name}',
+                        style: const TextStyle(fontSize: 14, color: Colors.black),
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        'Tipus: ${ubi.tipo}',
+                        style: const TextStyle(fontSize: 14, color: Colors.black),
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        'Horari: ${ubi.horari}',
+                        style: const TextStyle(fontSize: 14, color: Colors.black),
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        'Comentari: ${ubi.comentari}',
+                        style: const TextStyle(fontSize: 14, color: Colors.black),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            }).toList(),
           ],
         );
       }),
     );
   }
-}*/
-
-
+}
