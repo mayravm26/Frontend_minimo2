c4611edb8faba1a1def24f20211c90766c550c39
LuciaOcana <lucia.ocana@estudiantat.upc.edu>
2024-12-12 09:43:42 +0100

    detalls visuals

--- lib/screen/perfilScreen.dart
+++ lib/screen/perfilScreen.dart
@@ -33,67 +33,72 @@ class _PerfilScreenState extends State<PerfilScreen> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Perfil',
-          style: TextStyle(color: Colors.white),
-        ),
-        backgroundColor: const Color(0xFF89AFAF), // El mismo color que hemos usado
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: const Text(
+        'Perfil',
+        style: TextStyle(color: Colors.white),
       ),
-      body: Center( // Centra el contenido en la pantalla
-        child: SingleChildScrollView( // Asegura que el contenido sea desplazable si es necesario
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center, // Centra los elementos verticalmente
-              crossAxisAlignment: CrossAxisAlignment.center, // Centra los elementos horizontalmente
-              children: [
-                CircleAvatar(
-                  radius: 80, // Tamaño del avatar
-                  backgroundColor: Colors.grey[300], // Color de fondo del avatar
-                  child: Icon(
-                    Icons.person, // El ícono de Flutter que representa un perfil
-                    size: 80, // Tamaño del ícono
-                    color: Colors.white, // Color del ícono
-                  ),
+      backgroundColor: const Color(0xFF89AFAF), // El mismo color que hemos usado
+    ),
+    body: Center( // Centra el contenido en la pantalla
+      child: SingleChildScrollView( // Asegura que el contenido sea desplazable si es necesario
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center, // Centra los elementos verticalmente
+            crossAxisAlignment: CrossAxisAlignment.center, // Centra los elementos horizontalmente
+            children: [
+              CircleAvatar(
+                radius: 80, // Tamaño del avatar
+                backgroundColor: Colors.grey[300], // Color de fondo del avatar
+                child: Icon(
+                  Icons.person, // El ícono de Flutter que representa un perfil
+                  size: 80, // Tamaño del ícono
+                  color: Colors.white, // Color del ícono
                 ),
-                const SizedBox(height: 20),
-                // Nombre del usuario, si está disponible
-                Text(
-                  _username ?? 'Nombre del Usuario', // Usa el nombre del usuario desde SharedPreferences
-                  style: const TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Color(0xFF89AFAF), // El mismo color que hemos usado
-                  ),
+              ),
+              const SizedBox(height: 20),
+              // Nombre del usuario, si está disponible
+              Text(
+                _username ?? 'Nombre del Usuario', // Usa el nombre del usuario desde SharedPreferences
+                style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF89AFAF), // El mismo color que hemos usado
                 ),
-                const SizedBox(height: 10),
-                
-                // Correo electrónico, si está disponible
-                Text(
-                  _email ?? 'usuario@example.com', // Usa el correo desde SharedPreferences
-                  style: const TextStyle(
-                    fontSize: 16,
-                    color: Colors.grey,
-                  ),
+              ),
+              const SizedBox(height: 10),
+              
+              // Correo electrónico, si está disponible
+              Text(
+                _email ?? 'usuario@example.com', // Usa el correo desde SharedPreferences
+                style: const TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey,
                 ),
-                const SizedBox(height: 30),
-                
-                // Botones debajo de la foto de perfil
-                _buildProfileButton(context, 'Configuración'),
-                const SizedBox(height: 10),
-                //_buildProfileButton(context, 'Mis Posts'),
-                const SizedBox(height: 10),
-                _buildProfileButton(context, 'Cerrar Sesión'),
-              ],
-            ),
+              ),
+              const SizedBox(height: 30),
+              
+              // Botones debajo de la foto de perfil
+              SizedBox(
+                width: 200, // Ancho del botón Configuración
+                child: _buildProfileButton(context, 'Configuración'),
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                width: 200, // Ancho del botón Cerrar Sesión
+                child: _buildProfileButton(context, 'Cerrar Sesión'),
+              ),
+            ],
           ),
         ),
       ),
-    );
-  }
+    ),
+  );
+}
+
 
   // Widget para construir los botones con un estilo consistente
   Widget _buildProfileButton(BuildContext context, String text) {
